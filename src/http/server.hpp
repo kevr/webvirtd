9d36b65c7a5ce2f67b751abfee81c76832fbe4b0
Kevin Morris <kevr@0cost.org>
2023-01-22 16:44:58 -0800

    feat: add webvirt::http::server
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ src/http/server.hpp
@@ -0,0 +1,150 @@
+/* Copyright (C) 2023 Kevin Morris <kevr@0cost.org> */
+#ifndef HTTP_SERVER_HPP
+#define HTTP_SERVER_HPP
+
+#include "connection.hpp"
+#include "handlers.hpp"
+#include "io_service.hpp"
+#include <boost/asio.hpp>
+#include <boost/asio/local/stream_protocol.hpp>
+#include <boost/beast.hpp>
+#include <chrono>
+#include <filesystem>
+#include <functional>
+
+namespace webvirt::net
+{
+using unix = boost::asio::local::stream_protocol;
+};
+
+namespace webvirt::http
+{
+
+template <typename protocol_t>
+class server
+{
+private:
+    std::filesystem::path socket_path_;
+
+    bool io_owned_ = true;
+    io_service *io_ = nullptr;
+    typename protocol_t::acceptor acceptor_;
+    typename protocol_t::socket socket_;
+
+    std::chrono::milliseconds timeout_ = std::chrono::milliseconds(60 * 1000);
+
+    std::function<void(connection<net::unix::acceptor, net::unix::socket> &)>
+        on_accept_ = webvirt::http::on_accept<
+            connection<net::unix::acceptor, net::unix::socket>>;
+
+    std::function<void(
+        connection<net::unix::acceptor, net::unix::socket> &,
+        const boost::beast::http::request<boost::beast::http::dynamic_body> &,
+        boost::beast::http::response<boost::beast::http::dynamic_body> &)>
+        on_request_ = webvirt::http::on_request<
+            connection<net::unix::acceptor, net::unix::socket>>;
+
+    std::function<void()> on_close_ = webvirt::http::on_close;
+
+    std::function<void(const char *, beast::error_code)> on_error_ =
+        webvirt::http::on_error;
+
+public:
+    server(std::filesystem::path socket_path)
+        : socket_path_(std::move(socket_path))
+        , io_(new io_service())
+        , acceptor_(*io_, socket_path_.string())
+        , socket_(*io_)
+    {
+    }
+
+    server(io_service &io, std::filesystem::path socket_path)
+        : socket_path_(std::move(socket_path))
+        , io_owned_(false)
+        , io_(&io)
+        , acceptor_(*io_, socket_path_.string())
+        , socket_(*io_)
+    {
+    }
+
+    ~server()
+    {
+        if (io_owned_)
+            delete io_;
+    }
+
+    server &timeout(std::chrono::milliseconds ms)
+    {
+        timeout_ = ms;
+        return *this;
+    }
+
+    std::chrono::milliseconds timeout() const
+    {
+        return timeout_;
+    }
+
+    std::size_t run()
+    {
+        async_accept();
+        return io_->process();
+    }
+
+    void on_accept(std::function<
+                   void(connection<net::unix::acceptor, net::unix::socket> &)>
+                       fn)
+    {
+        on_accept_ = fn;
+    }
+
+    void on_request(
+        std::function<void(
+            connection<net::unix::acceptor, net::unix::socket> &,
+            const boost::beast::http::request<boost::beast::http::dynamic_body>
+                &,
+            boost::beast::http::response<boost::beast::http::dynamic_body> &)>
+            fn)
+    {
+        on_request_ = fn;
+    }
+
+    void on_error(std::function<void(const char *, beast::error_code)> fn)
+    {
+        on_error_ = fn;
+    }
+
+    void on_close(std::function<void()> fn)
+    {
+        on_close_ = fn;
+    }
+
+private:
+    void async_accept()
+    {
+        const auto func = __func__;
+        acceptor_.async_accept(
+            socket_, [this, &func](boost::beast::error_code) {
+                // Here, we don't handle beast::error_code; we let that
+                // job fall through to the connection we make. If there's
+                // a problem with the socket, operations will fails within
+                // the connection immediately, which calls on_error_.
+
+                auto conn = std::make_shared<
+                    connection<net::unix::acceptor, net::unix::socket>>(
+                    std::move(socket_), timeout_);
+                on_accept_(*conn);
+                conn->on_error(on_error_);
+                conn->on_close(on_close_);
+                conn->on_request(on_request_);
+                conn->start();
+
+                async_accept();
+            });
+    }
+};
+
+using unix_server = server<net::unix>;
+
+}; // namespace webvirt::http
+
+#endif /* HTTP_SERVER_HPP */
---
+++ src/http/server.test.cpp
@@ -0,0 +1,261 @@
+#include "server.hpp"
+#include "../syscaller.hpp"
+#include "../util.hpp"
+#include "client.hpp"
+#include <boost/beast/http/status.hpp>
+#include <gtest/gtest.h>
+#include <thread>
+
+using testing::Test;
+
+class server_test : public Test
+{
+protected:
+    static std::filesystem::path tmpdir, socket_path;
+
+    webvirt::io_service io;
+    webvirt::io_service client_io;
+
+    using server_t = webvirt::http::server<webvirt::net::unix>;
+    std::shared_ptr<server_t> server;
+
+public:
+    static void SetUpTestSuite()
+    {
+        tmpdir = socket_path = webvirt::make_tmpdir();
+        socket_path /= "socket.sock";
+    }
+
+    void SetUp() override
+    {
+        auto &sys = webvirt::syscaller::instance();
+        sys.fs_remove(socket_path);
+        std::cout << socket_path << std::endl;
+
+        server = std::make_shared<webvirt::http::server<webvirt::net::unix>>(
+            io, socket_path);
+    }
+
+    void TearDown() override
+    {
+        server.reset();
+    }
+
+    static void TearDownTestSuite()
+    {
+        auto &sys = webvirt::syscaller::instance();
+        sys.fs_remove_all(tmpdir);
+    }
+};
+
+std::filesystem::path server_test::tmpdir, server_test::socket_path;
+
+TEST_F(server_test, standalone_io)
+{
+    auto standalone_socket_path = tmpdir / "standalone.sock";
+    server_t standalone_server(standalone_socket_path.string());
+}
+
+TEST_F(server_test, runs_with_defaults)
+{
+    auto server_thread = std::thread([&] {
+        server->on_close([&] {
+            io.stop();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+
+    client->async_get("/");
+    client_io.process();
+
+    server_thread.join();
+}
+
+TEST_F(server_test, get)
+{
+    auto server_thread = std::thread([&] {
+        server->on_close([&] {
+            io.stop();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+
+    boost::beast::http::response<boost::beast::http::string_body> response;
+    client->on_response([&response](const auto &response_) {
+        response = response_;
+    });
+    client->async_get("/");
+    client_io.process();
+
+    server_thread.join();
+
+    auto request = client->request();
+    std::cout << request;
+
+    std::cout << response;
+    EXPECT_EQ(response.version(), webvirt::http::version::http_1_1);
+    EXPECT_EQ(response.result_int(),
+              static_cast<int>(boost::beast::http::status::ok));
+    EXPECT_EQ(response.has_content_length(), true);
+    EXPECT_EQ(response.at("connection"), "close");
+    EXPECT_EQ(response.at("server"), BOOST_BEAST_VERSION_STRING);
+}
+
+TEST_F(server_test, post)
+{
+    auto server_thread = std::thread([&] {
+        server->on_close([&] {
+            io.stop();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+    EXPECT_EQ(client->host(), "localhost");
+    EXPECT_EQ(client->version(), webvirt::http::version::http_1_1);
+
+    boost::beast::http::response<boost::beast::http::string_body> response;
+    client->on_response([&response](const auto &response_) {
+        response = response_;
+    });
+    client->async_post("/");
+    client_io.process();
+
+    server_thread.join();
+
+    auto request = client->request();
+    std::cout << request;
+
+    std::cout << response;
+    EXPECT_EQ(response.version(), webvirt::http::version::http_1_1);
+    EXPECT_EQ(response.result_int(),
+              static_cast<int>(boost::beast::http::status::ok));
+    EXPECT_EQ(response.has_content_length(), true);
+    EXPECT_EQ(response.at("connection"), "close");
+    EXPECT_EQ(response.at("server"), BOOST_BEAST_VERSION_STRING);
+}
+
+TEST_F(server_test, method_not_allowed)
+{
+    auto server_thread = std::thread([&] {
+        server->on_close([&] {
+            io.stop();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+
+    boost::beast::http::response<boost::beast::http::string_body> response;
+    client->on_response([&response](const auto &response_) {
+        response = response_;
+    });
+    client->async_options("/");
+    client_io.process();
+
+    server_thread.join();
+
+    EXPECT_EQ(
+        response.result_int(),
+        static_cast<int>(boost::beast::http::status::method_not_allowed));
+}
+
+TEST_F(server_test, client_connect_error)
+{
+    auto server_thread = std::thread([&] {
+        server->on_error([&](const char *, boost::beast::error_code) {
+            io.stop();
+        });
+        server->on_accept([&](auto &conn) {
+            conn.close();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+    client->async_get("/");
+    client_io.process();
+
+    server_thread.join();
+}
+
+TEST_F(server_test, read_error)
+{
+    auto server_thread = std::thread([&] {
+        server->on_error([&](const char *, boost::beast::error_code) {
+            io.stop();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+    client->on_connect([](auto &client) {
+        client.close();
+    });
+    client->async_get("/");
+    client_io.process();
+
+    server_thread.join();
+}
+
+TEST_F(server_test, write_error)
+{
+    auto server_thread = std::thread([&] {
+        server->on_error([this](const char *, boost::beast::error_code) {
+            io.stop();
+        });
+        server->on_request([&](auto &conn, const auto &, auto &) {
+            conn.close();
+        });
+        server->run();
+    });
+
+    using client_t = webvirt::http::client<webvirt::net::unix>;
+    std::shared_ptr<client_t> client =
+        std::make_shared<client_t>(client_io, socket_path.string());
+    client->on_error([&](const char *, boost::beast::error_code) {
+        client_io.stop();
+    });
+    client->async_get("/");
+    client_io.process();
+
+    server_thread.join();
+}
+
+TEST_F(server_test, deadline)
+{
+    using namespace webvirt;
+
+    auto server_thread = std::thread([&] {
+        server->timeout(std::chrono::milliseconds(10));
+        server->on_close([&] {
+            io.stop();
+        });
+        server->run();
+    });
+
+    net::unix::socket socket_(client_io);
+    socket_.async_connect(socket_path.string(), [](boost::beast::error_code) {
+    });
+    client_io.run_one();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    client_io.process();
+
+    server_thread.join();
+}
