dcc64e7f74f896709358f61efc9b46433dbe8f68
Kevin Morris <kevr@0cost.org>
2023-01-30 23:34:28 -0800

    change: reorder libvirt mock functions
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/mocks/libvirt.hpp
+++ src/mocks/libvirt.hpp
@@ -28,14 +28,14 @@ public:
     ~libvirt() = default;
 
     MOCK_METHOD(connect_ptr, virConnectOpen, (const char *));
+    MOCK_METHOD(std::vector<domain_ptr>, virConnectListAllDomains,
+                (connect_ptr, int));
     MOCK_METHOD(domain_ptr, virDomainLookupByName,
                 (connect_ptr, const char *));
     MOCK_METHOD(std::shared_ptr<char>, virDomainGetXMLDesc, (domain_ptr, int));
     MOCK_METHOD(int, virDomainGetState, (domain_ptr, int *, int *, int));
     MOCK_METHOD(int, virDomainGetID, (domain_ptr));
     MOCK_METHOD(const char *, virDomainGetName, (domain_ptr));
-    MOCK_METHOD(std::vector<domain_ptr>, virConnectListAllDomains,
-                (connect_ptr, int));
 };
 
 }; // namespace webvirt::mocks
