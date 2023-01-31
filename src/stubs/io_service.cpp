168225a1ea6becda9572284b3517226b9c4a3813
Kevin Morris <kevr@0cost.org>
2023-01-31 14:07:10 -0800

    fix: simplify io_service stub; don't run io_service at all
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/stubs/io_service.cpp
+++ src/stubs/io_service.cpp
@@ -23,8 +23,5 @@ webvirt::stubs::io_service::io_service(std::size_t iterations)
 
 std::size_t webvirt::stubs::io_service::process()
 {
-    for (std::size_t i = 0; i < iterations; ++i) {
-        this->run_one();
-    }
     return iterations;
 }
