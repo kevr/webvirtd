56c0e41df031f1c1c01b49fa45b3250839c2a6ef
Kevin Morris <kevr@0cost.org>
2023-02-05 04:16:40 -0800

    feat: add views::domains
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ src/views/domains.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2023 Kevin Morris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+#include "domains.hpp"
+#include "../json.hpp"
+#include "../virt/util.hpp"
+using namespace webvirt::views;
+
+void domains::index(virt::connection &conn, const std::string &,
+                    const std::smatch &, const http::request &,
+                    http::response &response)
+{
+    // Any response we send is JSON-serialized
+    response.set("Content-Type", "application/json");
+
+    Json::Value json(Json::arrayValue);
+    auto domains_ = conn.domains();
+    for (auto &domain : domains_) {
+        Json::Value map(Json::objectValue);
+        for (auto &kv : domain) {
+            map[kv.first] = kv.second;
+        }
+        json.append(map);
+    }
+
+    auto output = json::stringify(json);
+    response.body().append(output);
+    response.content_length(response.body().size());
+}
+
+void domains::show(virt::connection &conn, const std::string &,
+                   const std::smatch &location, const http::request &,
+                   http::response &response)
+{
+    const std::string name(location[1]);
+
+    // Any response we send is JSON-serialized
+    response.set("Content-Type", "application/json");
+
+    auto domain = conn.domain(name);
+    if (domain.get("id", "") == "") {
+        domain["detail"] = "Unable to find domain";
+        response.result(beast::http::status::not_found);
+    }
+
+    auto output = json::stringify(domain);
+    response.body().append(output);
+    response.content_length(response.body().size());
+}
+
+void domains::start(virt::connection &conn, const std::string &,
+                    const std::smatch &location, const http::request &,
+                    http::response &response)
+{
+    const std::string name(location[1]);
+    auto domain = conn.get_domain_ptr(name);
+    Json::Value data(Json::objectValue);
+    if (!conn.start(domain)) {
+        data["detail"] = "Unable to start domain";
+        response.result(beast::http::status::bad_request);
+        response.body().append(json::stringify(data));
+        response.content_length(response.body().size());
+        return;
+    }
+
+    data = simple_domain_json(domain);
+    response.result(beast::http::status::created);
+    response.body().append(json::stringify(data));
+    response.content_length(response.body().size());
+}
+
+void domains::shutdown(virt::connection &conn, const std::string &,
+                       const std::smatch &location, const http::request &,
+                       http::response &response)
+{
+    const std::string name(location[1]);
+    auto domain = conn.get_domain_ptr(name);
+    Json::Value data(Json::objectValue);
+
+    bool ok = false;
+    try {
+        ok = conn.shutdown(domain);
+    } catch (const std::out_of_range &exc) {
+        data["detail"] = "Shutdown operation timed out";
+        response.result(beast::http::status::gateway_timeout);
+        response.body().append(json::stringify(data));
+        response.content_length(response.body().size());
+        return;
+    }
+
+    if (!ok) {
+        data["detail"] = "Unable to shutdown domain";
+        response.result(beast::http::status::bad_request);
+        response.body().append(json::stringify(data));
+        response.content_length(response.body().size());
+        return;
+    }
+
+    data = simple_domain_json(domain);
+    response.result(beast::http::status::ok);
+    response.body().append(json::stringify(data));
+    response.content_length(response.body().size());
+}
+
+Json::Value domains::simple_domain_json(libvirt::domain_ptr domain)
+{
+    auto &lv = libvirt::ref();
+    int state, reason;
+    lv.virDomainGetState(domain, &state, &reason, 0);
+
+    Json::Value data(Json::objectValue);
+    int domain_id = lv.virDomainGetID(domain);
+    data["id"] = domain_id;
+    const char *name = lv.virDomainGetName(domain);
+    data["name"] = name;
+    data["state"] = Json::Value(Json::objectValue);
+    data["state"]["id"] = state;
+    data["state"]["string"] = virt::state_string(state);
+
+    return data;
+} // LCOV_EXCL_LINE
---
+++ src/views/domains.hpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 Kevin Morris
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+#ifndef VIEWS_DOMAINS_HPP
+#define VIEWS_DOMAINS_HPP
+
+#include "../http/namespaces.hpp"
+#include "../virt/connection.hpp"
+#include <boost/beast.hpp>
+
+#include <regex>
+
+namespace webvirt::views
+{
+
+class domains
+{
+public:
+    void index(virt::connection &, const std::string &, const std::smatch &,
+               const http::request &, http::response &);
+    void show(virt::connection &, const std::string &, const std::smatch &,
+              const http::request &, http::response &);
+    void start(virt::connection &, const std::string &, const std::smatch &,
+               const http::request &, http::response &);
+    void shutdown(virt::connection &, const std::string &, const std::smatch &,
+                  const http::request &, http::response &);
+
+private:
+    Json::Value simple_domain_json(libvirt::domain_ptr domain);
+};
+
+}; // namespace webvirt::views
+
+#endif /* VIEWS_DOMAINS_HPP */
